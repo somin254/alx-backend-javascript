bb48b0042fd6dc82bc6c388b46255840277ccacf
renson <somin254>
2024-07-29 21:03:27 +0300

    manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,12 @@
+/**
+ * get student list
+ * @return {{id: number, firstname : string, loction: string}[]}
+ */
+
+export default function getListStudents() {
+	return [
+	 { id: 1, firstName: 'Guillaume', location: 'San Francisco'},
+         { id: 2, firstName: 'James', location: 'Columbia'},
+         {  id: 5, firstName: 'Serena', location: 'San Francisco'}
+	];
+      }
