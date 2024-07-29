d6e79c59f1701c4512f911554996fd506b29d1eb
renson <somin254>
2024-07-29 21:25:11 +0300

    manipulation

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,11 +1,11 @@
 /**
- * get student list
- * @return {{id: number, firstname : string, loction: string}[]}
+ * Retrieves a list of students.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @returns {{id: Number, firstName: String, location: String}[]}
  */
-
 export default function getListStudents() {
   return [
-	 { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
